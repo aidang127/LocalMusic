a7b09de9027c34406aa1b0db5998cfa9b109fba1
aidang127 <47786212+aidang127@users.noreply.github.com>
2020-11-10 11:17:17 -0500

    Lab 6 Done!

--- app/models.py
+++ app/models.py
@@ -1,19 +1,45 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Artist(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    hometown = db.Column(db.String(64), index=True, unique=True)
+    hometown = db.Column(db.String(64), index=True)
     a2es = db.relationship("ArtistToEvent", backref="artist", lazy="dynamic")
 
 class Venue(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    address = db.Column(db.String(128), index=True)
+    city = db.Column(db.String(64), index=True)
+    state =db.Column(db.String(2), index=True)
     events = db.relationship("Event", backref="venue", lazy="dynamic")
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    date = db.Column(db.DateTime, index=True)
     venueID = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
     a2es = db.relationship("ArtistToEvent", backref="event", lazy="dynamic")
 
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 from flask import render_template, url_for, flash, redirect
 from app import app, db
 import random
-from app.forms import CreateArtistForm
-from app.models import Artist, Venue, Event, ArtistToEvent
+from app.forms import CreateArtistForm, LoginForm, RegistrationForm, CreateVenueForm, CreateEventForm
+from app.models import Artist, Venue, Event, ArtistToEvent, User
+from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
+from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/base')
+@login_required
 def base():
     return render_template('base.html', title='Home')
 '''
@@ -48,7 +53,44 @@ def newArtist():
             return redirect(url_for('Artists'))
     return render_template('newArtist.html',title="Create Artists", form=form)
 
+@app.route('/newVenue', methods=['GET', 'POST'])
+def newVenue():
 
+    form = CreateVenueForm()
+    if form.validate_on_submit():
+        v= Venue.query.filter_by(name=form.name.data).first()
+        if v is not None:
+            flash('Venue already exists')
+        else:
+            w = Venue(name=form.name.data, address=form.address.data, city=form.city.data, state=form.state.data)
+            flash('New Venue Created!')
+            db.session.add(w)
+            db.session.commit()
+            return redirect(url_for('Artists'))
+    return render_template('newVenue.html',title="Create Venues", form=form)
+
+@app.route('/newEvent', methods=['GET', 'POST'])
+def newEvent():
+
+    form = CreateEventForm()
+    form.venue.choices = [(v.id, v.name) for v in Venue.query.all()]
+    form.artists.choices = [(a.id, a.name) for a in Artist.query.all()]
+    if form.validate_on_submit():
+        e = Event.query.filter_by(name=form.name.data).first()
+        if e is not None:
+            flash('Event already exists')
+        else:
+            e = Event(name=form.name.data,date=form.date.data,
+                      venueID=form.venue.data)
+            flash('New Event Created!')
+            db.session.add(e)
+            db.session.commit()
+            for aid in form.artists.data:
+                a2e = ArtistToEvent(eventID=e.id,artistID=aid)
+                db.session.add(a2e)
+                db.session.commit()
+            return redirect(url_for('Artists'))
+    return render_template('newEvent.html',title="Create Events", form=form)
 @app.route('/populate_db')
 def populate_db():
     a1=Artist(name='Bastille', hometown="London")
@@ -61,11 +103,11 @@ def populate_db():
     db.session.add_all([a1, a2, a3, v1, v2, v3, v4])
     db.session.commit()
 
-    e1 =Event(name="IC Presents",venueID=v4.id)
-    e2 =Event(name="Rock the Park",venueID=v3.id)
-    e3 =Event(name="A&E Concerts",venueID=v2.id)
-    e4 =Event(name="State Theater Jam",venueID=v1.id)
-    e5 =Event(name="Music over Ithaca",venueID=v3.id)
+    e1 =Event(name="IC Presents",venueID=v4.id,date=datetime(2020,8,6,17,30))
+    e2 =Event(name="Rock the Park",venueID=v3.id,date=datetime(2020,8,6,17,30))
+    e3 =Event(name="A&E Concerts",venueID=v2.id,date=datetime(2020,8,6,17,30))
+    e4 =Event(name="State Theater Jam",venueID=v1.id,date=datetime(2020,8,6,17,30))
+    e5 =Event(name="Music over Ithaca",venueID=v3.id,date=datetime(2020,8,6,17,30))
     db.session.add_all([e1,e2,e3,e4,e5])
     db.session.commit()
 
@@ -75,16 +117,6 @@ def populate_db():
     AE4 = ArtistToEvent(eventID=e3.id, artistID=a2.id)
     db.session.add_all([AE1,AE2,AE3,AE4])
     db.session.commit()
-
-    '''
-    e1=Event(name="Rock the State", venueID=v1.id)
-    e2=Event(name="A&E Concerts", venueID=v2.id)
-    e3=Event(name="A Night at the Park", venueID=v3.id)
-    e4=Event(name="IC Presents", venueID=v4.id)
-    e5 = Event(name="Rock out Ithaca", venueID=v4.id)
-    db.session.add_all([e1,e2,e3,e4,e5])
-    db.session.commit()
-    '''
     return "Database has been populated"
 
 @app.route('/reset_db')
@@ -99,4 +131,36 @@ def reset_db():
    populate_db()
    return "Reset Database"
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('base'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=True)
+        return redirect(url_for('Artists'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('base'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('base'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
